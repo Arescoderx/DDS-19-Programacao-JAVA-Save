ba6f2a2082ec046752410b086bea9f72acbe97e5
Matheus Schmidt <matheus.schmidt@aluno.senai.br>
2024-08-11 20:49:51 -0300

    Atualizado

--- PROJETO_9_Matheus/src/main/java/com/mycompany/projeto_9_matheus/PROJETO_9_Matheus.java
+++ PROJETO_9_Matheus/src/main/java/com/mycompany/projeto_9_matheus/PROJETO_9_Matheus.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.projeto_9_matheus;
 
+import java.util.Random;
+
 /**
  *
  * @author mathe
@@ -11,58 +13,51 @@ package com.mycompany.projeto_9_matheus;
 public class PROJETO_9_Matheus{
 
     public static void main(String[] args){
+        int Tempo_Verde = 30;
+        int Tempo_Amarelo = 5;
+        int Tempo_Vermelho = 45;
+        int Ciclo_Total = Tempo_Verde + Tempo_Amarelo + Tempo_Vermelho;
+        int Tempo_Simu = 600;
+        int Limite_Cong = 10;
 
-        final int TEMPO_VERDE = 30;
-        final int TEMPO_AMARELO = 5;
-        final int TEMPO_VERMELHO = 45;
-        final int CICLO_TOTAL = TEMPO_VERDE + TEMPO_AMARELO + TEMPO_VERMELHO;
-        final int TEMPO_SIMULACAO = 600;
-        final int LIMITE_CONGESTIONAMENTO = 10;
+        Random random = new Random();
 
-        int carrosNaFila1 = 0;
-        int carrosPassaram1 = 0;
-        int tempoTotalEspera1 = 0;
-        int carrosCongestionados1 = 0;
-        int segundoAtual1 = 0;
+        int carrosNaFila1 = 0, carrosPassaram1 = 0, tempoTotalEspera1 = 0, carrosCongestionados1 = 0;
+        int carrosNaFila2 = 0, carrosPassaram2 = 0, tempoTotalEspera2 = 0, carrosCongestionados2 = 0;
+        int carrosNaFila3 = 0, carrosPassaram3 = 0, tempoTotalEspera3 = 0, carrosCongestionados3 = 0;
 
-        int carrosNaFila2 = 0;
-        int carrosPassaram2 = 0;
-        int tempoTotalEspera2 = 0;
-        int carrosCongestionados2 = 0;
-        int segundoAtual2 = 0;
+        int segundoAtual1 = 0, segundoAtual2 = 0, segundoAtual3 = 0;
 
-        int carrosNaFila3 = 0;
-        int carrosPassaram3 = 0;
-        int tempoTotalEspera3 = 0;
-        int carrosCongestionados3 = 0;
-        int segundoAtual3 = 0;
+        for (int segundo = 1; segundo <= Tempo_Simu; segundo++) {
+            segundoAtual1 = (segundoAtual1 + 1) % Ciclo_Total;
+            segundoAtual2 = (segundoAtual2 + 1) % Ciclo_Total;
+            segundoAtual3 = (segundoAtual3 + 1) % Ciclo_Total;
 
-        for (int segundo = 1; segundo <= TEMPO_SIMULACAO; segundo++) {
-            segundoAtual1 = (segundoAtual1 + 1) % CICLO_TOTAL;
-            segundoAtual2 = (segundoAtual2 + 1) % CICLO_TOTAL;
-            segundoAtual3 = (segundoAtual3 + 1) % CICLO_TOTAL;
-
-            String estadoSemaforo1 = (segundoAtual1 < TEMPO_VERDE) ? "Verde" : (segundoAtual1 < TEMPO_VERDE + TEMPO_AMARELO) ? "Amarelo" : "Vermelho";
-            String estadoSemaforo2 = (segundoAtual2 < TEMPO_VERDE) ? "Verde" : (segundoAtual2 < TEMPO_VERDE + TEMPO_AMARELO) ? "Amarelo" : "Vermelho";
-            String estadoSemaforo3 = (segundoAtual3 < TEMPO_VERDE) ? "Verde" : (segundoAtual3 < TEMPO_VERDE + TEMPO_AMARELO) ? "Amarelo" : "Vermelho";
+            String estadoSemaforo1 = (segundoAtual1 < Tempo_Verde) ? "Verde" : (segundoAtual1 < Tempo_Verde + Tempo_Amarelo) ? "Amarelo" : "Vermelho";
+            String estadoSemaforo2 = (segundoAtual2 < Tempo_Verde) ? "Verde" : (segundoAtual2 < Tempo_Verde + Tempo_Amarelo) ? "Amarelo" : "Vermelho";
+            String estadoSemaforo3 = (segundoAtual3 < Tempo_Verde) ? "Verde" : (segundoAtual3 < Tempo_Verde + Tempo_Amarelo) ? "Amarelo" : "Vermelho";
 
             if (segundo % 15 == 0) {
-                if (carrosNaFila1 < LIMITE_CONGESTIONAMENTO) {
-                    carrosNaFila1++;
+                int carrosChegando1 = random.nextInt(5) + 1;
+                int carrosChegando2 = random.nextInt(5) + 1;
+                int carrosChegando3 = random.nextInt(5) + 1;
+
+                if (carrosNaFila1 + carrosChegando1 <= Limite_Cong) {
+                    carrosNaFila1 += carrosChegando1;
                 } else {
-                    carrosCongestionados1++;
+                    carrosCongestionados1 += carrosChegando1;
                 }
 
-                if (carrosNaFila2 < LIMITE_CONGESTIONAMENTO) {
-                    carrosNaFila2++;
+                if (carrosNaFila2 + carrosChegando2 <= Limite_Cong) {
+                    carrosNaFila2 += carrosChegando2;
                 } else {
-                    carrosCongestionados2++;
+                    carrosCongestionados2 += carrosChegando2;
                 }
 
-                if (carrosNaFila3 < LIMITE_CONGESTIONAMENTO) {
-                    carrosNaFila3++;
+                if (carrosNaFila3 + carrosChegando3 <= Limite_Cong) {
+                    carrosNaFila3 += carrosChegando3;
                 } else {
-                    carrosCongestionados3++;
+                    carrosCongestionados3 += carrosChegando3;
                 }
             }
 
@@ -70,21 +65,18 @@ public class PROJETO_9_Matheus{
                 carrosNaFila1--;
                 carrosPassaram1++;
                 tempoTotalEspera1 += carrosNaFila1;
-                break;
             }
 
             while (estadoSemaforo2.equals("Verde") && carrosNaFila2 > 0) {
                 carrosNaFila2--;
                 carrosPassaram2++;
                 tempoTotalEspera2 += carrosNaFila2;
-                break;
             }
 
             while (estadoSemaforo3.equals("Verde") && carrosNaFila3 > 0) {
                 carrosNaFila3--;
                 carrosPassaram3++;
                 tempoTotalEspera3 += carrosNaFila3;
-                break;
             }
 
             System.out.println("Segundo " + segundo + ":");
@@ -93,24 +85,24 @@ public class PROJETO_9_Matheus{
             System.out.println("  Intersecao 3 - Semaforo: " + estadoSemaforo3 + ", Carros na fila: " + carrosNaFila3);
         }
 
-        System.out.println("Relatorio Final:");
+        System.out.println("Relatório Final:");
 
         double tempoMedioEspera1 = (carrosPassaram1 > 0) ? (double) tempoTotalEspera1 / carrosPassaram1 : 0;
         System.out.println("Intersecao 1:");
         System.out.println("  Total de carros que passaram: " + carrosPassaram1);
         System.out.println("  Tempo medio de espera: " + tempoMedioEspera1 + " segundos");
-        System.out.println("  Carros presos no congestionamento: " + carrosCongestionados1);
+        System.out.println("  Carros congestionados: " + carrosCongestionados1);
 
         double tempoMedioEspera2 = (carrosPassaram2 > 0) ? (double) tempoTotalEspera2 / carrosPassaram2 : 0;
         System.out.println("Intersecao 2:");
         System.out.println("  Total de carros que passaram: " + carrosPassaram2);
         System.out.println("  Tempo medio de espera: " + tempoMedioEspera2 + " segundos");
-        System.out.println("  Carros presos no congestionamento: " + carrosCongestionados2);
+        System.out.println("  Carros congestionados: " + carrosCongestionados2);
 
         double tempoMedioEspera3 = (carrosPassaram3 > 0) ? (double) tempoTotalEspera3 / carrosPassaram3 : 0;
         System.out.println("Intersecao 3:");
         System.out.println("  Total de carros que passaram: " + carrosPassaram3);
         System.out.println("  Tempo medio de espera: " + tempoMedioEspera3 + " segundos");
-        System.out.println("  Carros presos no congestionamento: " + carrosCongestionados3);
+        System.out.println("  Carros congestionados: " + carrosCongestionados3);
     }
 }
