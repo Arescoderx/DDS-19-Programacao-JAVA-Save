d3a89f508ccc9ce29d70e73074576741b9d37193
Matheus Schmidt <matheus.schmidt@aluno.senai.br>
2024-08-16 19:55:09 -0300

    08/16

--- Lista_de_Exercicios_Condicao_e_Repeticao/src/main/java/LISTA/Exercicio_19.java
+++ Lista_de_Exercicios_Condicao_e_Repeticao/src/main/java/LISTA/Exercicio_19.java
@@ -4,12 +4,24 @@
  */
 package LISTA;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mathe
  */
 public class Exercicio_19 {
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Por favor, insira a base:");
+        float base = ler.nextFloat();
         
+        System.out.println("Agora, insira o expoente:");
+        float expoente = ler.nextFloat();
+        
+        System.out.println("\nCalculando "+base+" elevado a "+expoente+"...\n");
+        
+        float resultado = (float) Math.pow(base,expoente);
+        System.out.println("O resultado de "+base+" elevado a "+expoente+" é "+resultado+".");
     }
 }
